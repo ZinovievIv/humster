4874edd80e97178057a92027d30410221289211a
ZinovievIv <103179359+ZinovievIv@users.noreply.github.com>
2023-04-06 21:30:03 +0300

    Humster

--- Main.java
+++ Main.java
@@ -13,7 +13,7 @@ public class Practicum {
 class HamsterFactory {
     Scanner scanner = new Scanner(System.in);
     ArrayList<Hamster> humsterHome = new ArrayList<>(); // нужна структура данных, в которой можно хранить хомяков
-     // мы не знаем, сколько будет хомяков: 10 или 10_000, поэтому нужна сущность, которая может изменять свой размер
+    // мы не знаем, сколько будет хомяков: 10 или 10_000, поэтому нужна сущность, которая может изменять свой размер
 
     public void start() {
         System.out.println("Добро пожаловать на фабрику хомяков!");
@@ -38,8 +38,8 @@ class HamsterFactory {
             } else {
                 executeCommand(command);
             } // если пользователь ввел команду "Завершить", надо закончить работу программы (остановить цикл)
-             // при завершении работы вывести сообщение "Программа завершена! Спасибо, что пользуетесь нашей сетью хомячих фабрик"
-             // иначе вызвать метод executeCommand(), который распознает команду и вызывает нужный метод для её обработки
+            // при завершении работы вывести сообщение "Программа завершена! Спасибо, что пользуетесь нашей сетью хомячих фабрик"
+            // иначе вызвать метод executeCommand(), который распознает команду и вызывает нужный метод для её обработки
         }
     }
 
@@ -52,13 +52,13 @@ class HamsterFactory {
     }
 
     private void executeCreate(String name) {
-         Humster name = new Humster(name);// создайте хомяка с именем [name] и добавьте его в ваше хранилище
+        Humster name = new Humster(name);// создайте хомяка с именем [name] и добавьте его в ваше хранилище
         humsterHome.add(name);
         System.out.println("Хомяк " + "'" + name + "'" + " создан и добавлен в хранилище");// выведите сообщение: "Хомяк '[name]' создан и добавлен в хранилище"
     }
 
     private void executeRemoveByIndex(int index) {
-        for (int i = 0; i < hamsterHome.size(); i++) { 
+        for (int i = 0; i < hamsterHome.size(); i++) {
             if (i == index) {
                 System.out.println("Хомяк " + "'" + humsteHome.get(index) + "'" + " удален успешно");
                 hamsterHome.remove(index);
@@ -66,7 +66,7 @@ class HamsterFactory {
                 System.out.println("Хомяк по заданному индексу не существует");
             }
         } // если хомяк существует, надо удалить его и вывести сообщение: "Хомяк '[Имя удаленного хомяка]' удалён успешно"
-         // иначе вывести сообщение: "Хомяка по заданному индексу не существует"
+        // иначе вывести сообщение: "Хомяка по заданному индексу не существует"
     }
 
     private void executeClear() {
@@ -78,27 +78,27 @@ class HamsterFactory {
         Humster newHumster = new Humster(name); // создайте хомяка с именем [name] и замените им хомяка на позиции [index]
         System.out.println("Хомяк " + "'" + humsterHome.get(index) + "'" + " был успешно заменён на хомяка " + "'" + name + "'");
         humserHome.set(index, name);// выведите сообщение "Хомяк '[Имя удаленного хомяка]' был успешно заменён на хомяка '[Имя нового хомяка]'"
-            
+
     }
 
     private void executeSize() {
         if (humsterHome.size() > 0) {
-        System.out.println("Количество хомяков в хранилище равно " + humsterHome.size());    
+            System.out.println("Количество хомяков в хранилище равно " + humsterHome.size());
         } else {
             System.out.println("В хранилище нет хомяков");// если в хранилище есть хомяки, надо вывести сообщение: "Количество хомяков в хранилище равно [текущее количество хомяков в хранилище]"
-         // иначе вывести: "В хранилище нет хомяков"
+            // иначе вывести: "В хранилище нет хомяков"
+        }
+
+        private void showErrorMessage() {
+            System.out.println("Неверная команда, попробуйте ещё раз.");// выведите сообщение: "Неверная команда, попробуйте ещё раз."
+        }
     }
 
-    private void showErrorMessage() {
-        System.out.println("Неверная команда, попробуйте ещё раз.");// выведите сообщение: "Неверная команда, попробуйте ещё раз."
+    class Hamster {
+
+        String name;
+
+        public Hamster(String name) {
+            this.name = name;
+        }
     }
-}
-
-class Hamster {
-
-    String name;
-
-    public Hamster(String name) {
-        this.name = name;
-    }
-}
